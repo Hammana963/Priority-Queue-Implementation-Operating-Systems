7e6677ac5bae0e10a45b3f1eb5f1df1e38e10f80
justinnguyen22 <80489273+justinnguyen22@users.noreply.github.com>
2022-05-07 11:53:29 -0700

    Update driver
    Updated driver to get rid of compiling errors

--- driver.c
+++ driver.c
@@ -1,3 +1,11 @@
+/**
+ * Driver.c
+ *
+ * Schedule is in the format
+ *
+ *  [name] [priority] [CPU burst]
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,9 +14,7 @@
 #include "list.h"
 #include "schedulers.h"
 
-using namespace std;
-
-#define SIZE    100
+#define SIZE 100
 
 int main(int argc, char *argv[])
 {
@@ -20,16 +26,17 @@ int main(int argc, char *argv[])
     int priority;
     int burst;
 
-    in = fopen(argv[1],"r");
-    
-    while (fgets(task,SIZE,in) != NULL) {
+    in = fopen(argv[1], "r");
+
+    while (fgets(task, SIZE, in) != NULL)
+    {
         temp = strdup(task);
-        name = strsep(&temp,",");
-        priority = atoi(strsep(&temp,","));
-        burst = atoi(strsep(&temp,","));
+        name = strsep(&temp, ",");
+        priority = atoi(strsep(&temp, ","));
+        burst = atoi(strsep(&temp, ","));
 
         // add the task to the scheduler's list of tasks
-        add(name,priority,burst);
+        add(name, priority, burst);
 
         free(temp);
     }
