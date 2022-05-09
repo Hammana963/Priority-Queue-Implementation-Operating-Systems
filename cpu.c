ec8245a4e7380190b3c84f7ef0321f8f8e80059a
justinnguyen22 <80489273+justinnguyen22@users.noreply.github.com>
2022-05-08 21:15:53 -0700

    Switch to Java
    Switched from c to java for project 4

--- cpu.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#include "task.h"
-
-// run this task for the specified time slice
-void run(Task *task, int slice) {
-    printf("Running task = [%s] [%d] [%d] for %d units.\n",task->name, task->priority, task->burst, slice);
-}
